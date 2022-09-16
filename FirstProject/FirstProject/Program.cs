2c8091196d72c49e9ed36691c57288f82e181cba
S H A L I Q <armanshaliq11@gmail.com>
2022-09-16 15:59:45 +0530

    adsdads

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("this is the second message");
